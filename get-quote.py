375ede61d9d3f9f9e0b0725de15aef1ab245f7d8
Maxwex <maxx.wexx@gmail.com>
2021-03-28 16:47:12 +0200

    Renamed the primary function

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,4 @@
-def main():
+def main5():
   print("Keep it logically awesome.")
 
   #f = open("quotes.txt")
@@ -8,4 +8,4 @@ def main():
   #print(quotes)
 
 if __name__== "__main__":
-  main()
+  main5()
